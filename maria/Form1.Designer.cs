ff5180fac9e502b6f18105860f6fd28a4e2fa028
Buzina <mariabuzinaa@yandex.ru>
2025-01-16 00:00:34 +0300

    CLOSE #1 Прототипирование интерфейса в Figma

--- maria/Form1.Designer.cs
+++ maria/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace maria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 500);
+            this.ClientSize = new System.Drawing.Size(615, 507);
             this.Name = "Form1";
             this.Text = "Авторизация";
             this.ResumeLayout(false);

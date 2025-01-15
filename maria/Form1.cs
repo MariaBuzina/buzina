af535c805bc693c5a190c22870cab790901ff9f5
Buzina <mariabuzinaa@yandex.ru>
2025-01-16 01:05:50 +0300

    приветики

--- maria/Form1.cs
+++ maria/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,46 @@ namespace maria
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "user" && textBox2.Text == "user")
+            {
+                MainForm mainForm = new MainForm();
+                this.Visible = false;
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка авторизации!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] captchaFile;
+            captchaFile = Directory.GetFiles(@".\Resources\", "*.png");
+
+            Random random = new Random();
+            int index = random.Next(captchaFile.Length);
+            pictureBox1.Image = Image.FromFile(captchaFile[index]);
+        }
+
+        private bool isTextVisible = false;
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            isTextVisible = !isTextVisible; // Переключаем состояние
+
+            if (isTextVisible)
+            {
+                textBox2.PasswordChar = default; // Показываем текст
+            }
+            else
+            {
+                textBox2.PasswordChar = '*'; // Скрываем текст
+            }
         }
     }
 }
--- maria/Properties/Resources.Designer.cs
+++ maria/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace maria.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap image_2 {
+            get {
+                object obj = ResourceManager.GetObject("image 2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
